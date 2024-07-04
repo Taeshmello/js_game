82501f14fe5294efc9bbae6dc73727c5f8a9e2a6
Taeshmello <taeho060419@gmail.com>
2024-07-04 14:12:15 +0900

    떨어지는 효과음 추가 및 전체 크기 증가

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { FRUITS } from "./fruits.js";
 
-
 const Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -18,45 +17,40 @@ const render = Render.create({
     element: document.body,
     options: {
         wireframes : false,
-        background: '#F7F4C8', // 배경
+        background: "linear-gradient(143deg, #ace2f1, #00c2da, #006e91, #003f55, #001e28, black)", // 배경
         width: 620,
-        height: 850,
-
+        height: 1000,
     }
 });
 
-
 const world = engine.world;
 
-const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+const leftWall = Bodies.rectangle(15, 610, 30, 2000, {
     isStatic : true, //고정기능
-    render : {fillStyle : "#E6B143"}
-})
+    render : {fillStyle : "#38EDDB"}
+});
 
-const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+const rightWall = Bodies.rectangle(605, 610, 30, 2000, {
     isStatic : true, //고정기능
-    render : {fillStyle : "#E6B143"}
-})
+    render : {fillStyle : "#38EDDB"}
+});
 
-const ground = Bodies.rectangle(310, 820, 620, 60,{
+const ground = Bodies.rectangle(310, 1000, 620, 60,{
     isStatic : true,
-    render : {fillStyle: '#E6B143'}
-})
+    render : {fillStyle: "#38EDDB"}
+});
 
 const endLine = Bodies.rectangle(310, 150, 620, 2, {
     name : "endLine",
     isStatic : true, //고정기능
     isSensor : true, // 충돌은 감지하나 물리엔진은 적용 안 함
-    render : {fillStyle : "#E6B143"}
-})
+    render : {fillStyle : "#38EDDB"}
+});
 
-World.add(world, [leftWall, rightWall, ground, endLine])
+World.add(world, [leftWall, rightWall, ground, endLine]);
 
 Render.run(render);
 Runner.run(engine);
-
-
-
 
 //현재 과일 값을 저장할 변수 생성
 let currentBody;
@@ -65,16 +59,11 @@ let currentFruit;
 //키 조작 제어 변수
 let disableAction = false;
 
-
 // 과일 떨어지는 함수
-
 const addFruit = () => {
-
     // 과일 배열 저장
     const index = Math.floor(Math.random() * 5);
-
     const fruits = FRUITS[index];
-
     const body = Bodies.circle(300, 50, fruits.radius, {
         index : index,
         isSleeping : true, // 떨어짐 대기
@@ -86,88 +75,88 @@ const addFruit = () => {
 
     //현재 과일값 저장
     currentBody = body;
-    currentFruit = fruits
+    currentFruit = fruits;
+    World.add(world, body);
+};
 
-    World.add(world, body)
+// 소리를 재생하는 함수
+const playSound = async (url, duration) => {
+    const audio = new Audio("falling.mp3");
+    audio.loop = false;
+    audio.volume = 0.5;
+    await audio.play();
+    await new Promise(resolve => setTimeout(resolve, duration));
+    audio.pause();
+};
 
-   
+window.onkeydown = async (event) => {
+    //제어 조작 변수가 true 일 경우 바로 리턴
+    if(disableAction){
+        return;
     }
 
-    window.onkeydown = (event) => {
-
-        //제어 조작 변수가 true 일 경우 바로 리턴
-        if(disableAction){
-            return;
-        }
-
-        switch(event.code) {
-            case "KeyA":
-                if(currentBody.position.x - currentFruit.radius > 30)
-                Body.setPosition(currentBody, {
-                    x: currentBody.position.x - 10,
-                    y: currentBody.position.y
-                })
-                break;
-            case "KeyD":
-                if(currentBody.position.x + currentFruit.radius < 570)
-                Body.setPosition(currentBody,{
-                    x: currentBody.position.x + 10,
-                    y: currentBody.position.y
-                })
-                break;
-            case "KeyS":
-                currentBody.isSleeping = false;
-                disableAction = true;
-
-                setTimeout(()=>{
-                    addFruit();
-                    disableAction = false;
-                }, 1000);
-                break;
-        }
-    }
-
-    Events.on(engine, "collisionStart",(event) => {
-        // 콜리전 이벤트 발생시 생기는 모든 오브젝트를 비교
-        event.pairs.forEach((collision) => {
-            if(collision.bodyA.index == collision.bodyB.index){
-                
-                // 기존 과일의 index를 저장
-                const index = collision.bodyA.index;
-
-                // 수박일 경우 처리 안함
-                if(index === FRUITS.length - 1){
-                    return;
-                }
-
-
-                // 충돌이 일어나는 같은 과일 제거
-                World.remove(world, [collision.bodyA, collision.bodyB]);
-
-                const newFruit = FRUITS[index + 1];
-                const newBody = Bodies.circle(
-                    // 부딪친 위치의 x,y 값
-                    collision.collision.supports[0].x,
-                    collision.collision.supports[0].y,
-                    newFruit.radius,
-                    {
-                        // 과일 index 저장
-                        index: index + 1,
-                        // 새로운 과일 랜더링
-                        render : { sprite: { texture: `${newFruit.name}.png`}},
-
-                    }
-                );
-
-                World.add(world, newBody);
-            }
-            if(!disableAction && (collision.bodyA.name == "endLine" || collision.bodyB.name === "endLine")){
-            alert("Game Over");
+    switch(event.code) {
+        case "KeyA":
+            if(currentBody.position.x - currentFruit.radius > 30)
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x - 10,
+                y: currentBody.position.y
+            });
+            break;
+        case "KeyD":
+            if(currentBody.position.x + currentFruit.radius < 570)
+            Body.setPosition(currentBody, {
+                x: currentBody.position.x + 10,
+                y: currentBody.position.y
+            });
+            break;
+        case "KeyS":
+            currentBody.isSleeping = false;
             disableAction = true;
-        }
-        });
+            await playSound("falling.mp3", 1500);
+            addFruit();
+            disableAction = false;
+            break;
+    }
+};
 
-        
-    })
+Events.on(engine, "collisionStart", (event) => {
+    // 콜리전 이벤트 발생시 생기는 모든 오브젝트를 비교
+    event.pairs.forEach((collision) => {
+        if(collision.bodyA.index == collision.bodyB.index){
+            // 기존 과일의 index를 저장
+            const index = collision.bodyA.index;
+
+            // 수박일 경우 처리 안함
+            if(index === FRUITS.length - 1){
+                return;
+            }
+
+            // 충돌이 일어나는 같은 과일 제거
+            World.remove(world, [collision.bodyA, collision.bodyB]);
+
+            const newFruit = FRUITS[index + 1];
+            const newBody = Bodies.circle(
+                // 부딪친 위치의 x,y 값
+                collision.collision.supports[0].x,
+                collision.collision.supports[0].y,
+                newFruit.radius,
+                {
+                    // 과일 index 저장
+                    index: index + 1,
+                    // 새로운 과일 랜더링
+                    render : { sprite: { texture: `${newFruit.name}.png`}},
+                }
+            );
+
+            World.add(world, newBody);
+
+            if(!disableAction && (collision.bodyA.name == "endLine" || collision.bodyB.name === "endLine")){
+                alert("Game Over");
+                disableAction = true;
+            }
+        }
+    });
+});
 
 addFruit();
